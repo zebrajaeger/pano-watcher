5be0f7c1681a5672ecadd3f92440dc9da4ff77b0
Lars Brandt <zebrajaeger@zebrajaeger.de>
2018-12-02 15:59:21 +0100

    add popup on click (without content)

--- client/js/app/app.js
+++ client/js/app/app.js
@@ -6,3 +6,18 @@ function slugify(text) {
         .replace(/^-+/, '')             // Trim - from start of text
         .replace(/-+$/, '');            // Trim - from end of text
 }
+
+$(document).ready(function () {
+    $("body").on("click", ".grid-item", function () {
+        console.log("!!!", this)
+        $("body").addClass("popup-open").fadeIn(2000);
+        $('.overlay-popup').addClass("popup-open").fadeIn(400);
+    });
+
+// close/hide popup (+overlay)
+    $.fn.popupClose = function () {
+        $("body").removeClass("popup-open")
+        $(".overlay-popup").removeClass("popup-open");
+        return this;
+    };
+});
